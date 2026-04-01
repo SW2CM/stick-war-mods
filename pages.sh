710db303c66f7ad427e4a6568e816ea62bee60b3
RinasSam <63971076+RinasSam@users.noreply.github.com>
2026-04-01 18:00:08 +0300

    Move index.json to mods/

--- pages.sh
+++ pages.sh
@@ -7,6 +7,7 @@ git restore --source master mods/
 git restore --source master fusion.sh
 find ./ -type f -name "*.swf" -delete;
 ./fusion.sh
+mv index.json mods/
 git add mods
-git add index.json
 git commit -m "Automatically generated from the master branch"
+# git checkout master --force
