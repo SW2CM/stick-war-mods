39095d723f7200047d4603e7a86e9e65e1bb9562
RinasSam <63971076+RinasSam@users.noreply.github.com>
2026-04-01 18:10:06 +0300

    pages.sh now builds the project as well

--- pages.sh
+++ pages.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 #
 # pages.sh: dynamically creates a directory suitable for deployment on the web.
+cd app
+lime clean html5
+lime build html5 -final
+cd ..
 git checkout gh-pages
 git rm --cached -r .
 git restore --source master mods/
@@ -8,6 +12,8 @@ git restore --source master fusion.sh
 find ./ -type f -name "*.swf" -delete;
 ./fusion.sh
 mv index.json mods/
+git restore --source master app/build/openfl/html5/bin
+find ./app/build/openfl/html5/bin/ -type f -exec mv '{}' ./ \;
 git add mods
 git commit -m "Automatically generated from the master branch"
-# git checkout master --force
+git checkout master
