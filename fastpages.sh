93c82a2f0a1d09ada552d874bceebd0a887888fd
RinasSam <63971076+RinasSam@users.noreply.github.com>
2026-04-07 17:36:20 +0300

    Fastpages update

--- fastpages.sh
+++ fastpages.sh
@@ -10,9 +10,6 @@ find ./ -type f -name "*.swf" -delete;
 ./fusion.sh
 git rm --cached fusion.sh
 mv index.json mods/
-git restore --source master app/build/openfl/html5/bin
-find ./app/build/openfl/html5/bin/ -type f -exec mv '{}' ./ \;
-git add mods favicon.png index.html sw2cmbrowser.js
 echo Pushing...
 git commit -m "Automatically generated from the master branch"
 git push
